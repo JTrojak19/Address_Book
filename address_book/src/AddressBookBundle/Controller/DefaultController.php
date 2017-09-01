a85d311cc046ce23b84d318988feb4ee79e9beb9
Joanna Trojak <joannatrojak19@gmail.com>
2017-09-01 12:14:15 +0200

    add form view

--- address_book/src/AddressBookBundle/Controller/DefaultController.php
+++ address_book/src/AddressBookBundle/Controller/DefaultController.php
@@ -4,6 +4,9 @@ namespace AddressBookBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response; 
+use AddressBookBundle\Entity\Contact; 
 
 class DefaultController extends Controller
 {
@@ -13,5 +16,20 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('AddressBookBundle:Default:index.html.twig');
+    }
+    /**
+     * @Route("/new")
+     * 
+     */
+    public function addNewEntityAction(Request $request)
+    {
+        $request->request->get('new'); 
+        $contact = new Contact(); 
+        $form = $this->createFormBuilder($contact)
+                ->add('name', 'text')
+                ->add('surname', 'text')
+                ->add('description', 'text')
+                ->getForm();       
+        return $this->render('AddressBookBundle:Default:new.html.twig', array('form'=>$form->createView())); 
     }
 }
